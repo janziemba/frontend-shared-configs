2f0f2f473fe7d9430e30b3ac9b53a6276b488885
Jan Ziemba <ziemba.jan@icloud.com>
2022-04-24 17:05:20 +0200

    Chore: Remove scope from Semantic Release commits

--- .releaserc.js
+++ .releaserc.js
@@ -22,7 +22,7 @@ module.exports = {
             '@semantic-release/git',
             {
                 assets: ['CHANGELOG.md', 'package.json'],
-                message: 'Chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+                message: 'Chore: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
             },
         ],
     ],
